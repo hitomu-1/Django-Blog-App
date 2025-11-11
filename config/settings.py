6080bcf48b4db90b6ca2d79675f0e53d68abee53
hitomu-1 <nnndmo.mtoaid@gmail.com>
2025-11-12 00:17:38 +0900

    仮デプロイ用にsettings.pyを修正、requirements.txtを作成

--- config/settings.py
+++ config/settings.py
@@ -10,17 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-fc%lt+)(s^^%g!f%6__$har71-9j3tmh%m3+)i!p0b4h!&*cux"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
